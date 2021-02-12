3c994b9b3bf3246005a75476282729d2b4d186b9
rumi <maziar9170@gmail.com>
2021-02-12 23:35:34 +0330

    Initial commit by rumi

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+function Area () {
+    var b=document.getElementById("b").value;
+    var h=document.getElementById("h").value;
+    var x =h*b/2;
+    document.querySelector(".demo").innerHTML=x;
+    console.log(b,h)
+    }
+    
